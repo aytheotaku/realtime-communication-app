0e601ff03daf5c5018ddc521f31d8b548b59c32f
Ayomide Akinbo <ayakinbo@gmail.com>
2023-08-16 22:31:53 +0100

    setting views properly

--- app.js
+++ app.js
@@ -17,6 +17,7 @@ app.use(morgan('dev'))
 app.use(router)
 app.use('/documentation', swaggerDocUI.serve)
 app.use('/documentation', swaggerDocUI.setup(swaggerDoc))
+app.set('views', path.join(__dirname, './views'));
 app.set('view engine', 'ejs')
 
 
